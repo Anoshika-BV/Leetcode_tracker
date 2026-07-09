5784c31b99dfd0059a74bf702babc413931e0531
Anoshika B V <anoshika2004@gmail.com>
2026-07-09 09:15:47 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:15:46 AM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //test
+        if (s.length() != t.length())
+            return false;
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+
+        for (int check : arr) {
+            if (check != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
