6e20f5605234c675a65e3cb48fb2ba90f5128c09
Anoshika B V <anoshika2004@gmail.com>
2026-07-09 09:15:25 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 9:15:24 AM
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int cookiesNums = s.length;
+        //test
+        if(cookiesNums == 0)  return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int maxNum = 0;
+        int cookieIndex = cookiesNums - 1;
+        int childIndex = g.length - 1;
+        while(cookieIndex >= 0 && childIndex >=0){
+            if(s[cookieIndex] >= g[childIndex]){
+                maxNum++;
+                cookieIndex--;
+                childIndex--;
+            }
+            else{
+                childIndex--;
+            }
+        }
+
+        return maxNum;
+    }
+}
