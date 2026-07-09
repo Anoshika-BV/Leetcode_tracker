fc7c68c9683ebc30e61bab28b73cc65aea8bdc31
Anoshika B V <anoshika2004@gmail.com>
2026-07-09 09:16:15 +0530

    Create 167-TwoSumIiInputArrayIsSorted

---
+++ 167-TwoSumIiInputArrayIsSorted/167-TwoSumIiInputArrayIsSorted.java
@@ -0,0 +1,25 @@
+// Last updated: 7/9/2026, 9:16:15 AM
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++){
+            int low = i+1;
+            int high = nums.length -1;
+            int mid;
+            int complement = target - nums[i];
+            
+            while(low <= high){
+                mid = (low + high) >>> 1;
+                if(nums[mid] == complement && mid != i){
+                    return new int[]{i+1, mid+1};
+                }
+                else if(nums[mid] < complement){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return null;
+    }
+}
