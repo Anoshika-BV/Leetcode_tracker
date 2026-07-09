db9e076db64b693b2eb59c018192186332741f2d
Anoshika B V <anoshika2004@gmail.com>
2026-07-09 09:15:02 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:15:01 AM
+class Solution {
+    public int fib(int n) {
+        //test
+        if (n <= 1) return n;
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b;
+    }
+}
