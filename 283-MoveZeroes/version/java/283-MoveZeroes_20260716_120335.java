ead5f9725648b0d619572c6587e1118b6cacbdd9
Anoshika B V <anoshika2004@gmail.com>
2026-07-16 12:03:36 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/version/java/283-MoveZeroes_20260716_120335.java
@@ -0,0 +1,17 @@
+// Last updated: 7/16/2026, 12:03:35 PM
+1class Solution {
+2    public void moveZeroes(int[] nums) {
+3        //test
+4        int index = 0;
+5        for (int i = 0; i < nums.length; i++) {
+6            if (nums[i] != 0) {
+7                nums[index] = nums[i];
+8                index++;
+9            }
+10        }
+11        while (index < nums.length) {
+12            nums[index] = 0;
+13            index++;
+14        }
+15    }
+16}
