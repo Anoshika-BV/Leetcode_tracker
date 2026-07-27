5e377c7d634e46a3ce4445260b2661f3f4a94de9
Anoshika B V <anoshika2004@gmail.com>
2026-07-27 09:32:32 +0530

    Create 292-NimGame

---
+++ 292-NimGame/version/java/292-NimGame_20260727_093231.java
@@ -0,0 +1,10 @@
+// Last updated: 7/27/2026, 9:32:31 AM
+1class Solution {
+2    public boolean canWinNim(int n) {
+3        //test
+4        if (n % 4 != 0)
+5            return true;
+6        else
+7            return false;
+8    }
+9}
